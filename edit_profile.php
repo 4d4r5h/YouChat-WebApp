4489f3ca0e70fb2de133ec34d702180491cf8de9
Sai Vardhan Vemulapalli <76389752+asta3002@users.noreply.github.com>
2023-05-10 02:50:12 +0530

    Updated edit_profile.php for CSS

--- edit_profile.php
+++ edit_profile.php
@@ -90,17 +90,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>YouChat</title>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" />
-    <!-- <link rel="stylesheet" href="index.css"> -->
+     <link rel="stylesheet" href="edit_profile.css"> 
     <link href="https://cdn.usebootstrap.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
-<style>
-    body {
-        margin-top: 20px;
-        background: #f8f8f8
-    }
-</style>
+
 
 <body>
     <div class="container">
@@ -137,41 +132,52 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <div class="card">
                             <div class="card-body">
                                 <div class="e-profile">
-                                    <div class="row">
-                                        <div class="col-12 col-sm-auto mb-3">
-                                            <div class="mx-auto" style="width: 140px;">
-                                                <div class="d-flex justify-content-center align-items-center rounded" style="height: 140px; background-color: rgb(233, 236, 239);">
-                                                    <!-- <span style="color: rgb(166, 168, 170); font: bold 8pt Arial;">140x140</span> -->
-                                                    <img height="140px" width="140px" style="object-fit:contain;" src='<?php echo $image ?>' />
-                                                </div>
+                                    
+                                        <div class=header>    
+                                            <div class="row">
+                                                    <div class="col-12 col-sm-auto mb-3">
+                                                        <div class="mx-auto" style="width: 140px;">
+                                                            <div class="d-flex  rounded" >
+                                                                <!-- <span style="color: rgb(166, 168, 170); font: bold 8pt Arial;">140x140</span> -->
+                                                                <img class="round" height="140px" width="140px"  src='<?php echo $image ?>' />
+                                                            </div>
+                                                        </div>
+                                                        <div class="mt-3">
+                                                                <form action="edit_picture.php" method="POST" enctype="multipart/form-data">
+                                                                    <!-- Select Image File to Upload: -->
+                                                                    <input id="profile_picture" type="file" name="file" style="color:transparent; width:150.px;"/>
+                                                                    <br>
+                                                                    <!-- <input type="submit" name="submit" value="Upload"> -->
+                                                                    <button id="Change_pic"class="btn btn-primary" type="submit">
+                                                                        <i class="fa fa-fw fa-camera"></i>
+                                                                        <span>Change Photo</span>
+                                                                    </button>
+                                                                </form>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
+                                                        <div class="text-center text-sm-left mb-2 mb-sm-0">
+                                                        
+                                                            <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap"><b><?php echo $fullname ?></b></h4>
+                                                            <p class="mb-0"><?php echo $username ?></p>
+                                                            <div class="text-muted"><small>Last Online : <?php echo $date . " - " . $time ?></small></div>
+                                                            
+                                                            <div class="col d-flex justify-content-end">
+                                                                <button class="btn btn-primary" type="submit">Save Changes</button>
+                                                            </div>
+                                                            
+                                                        </div>
+                                                        <!-- <div class="text-center text-sm-right">
+                                                            <span class="badge badge-secondary">administrator</span>
+                                                            <div class="text-muted"><small>Joined 09 Dec 2017</small></div>
+                                                        </div> -->
+                                                    </div>
                                             </div>
                                         </div>
-                                        <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
-                                            <div class="text-center text-sm-left mb-2 mb-sm-0">
-                                                <h4 class="pt-sm-2 pb-1 mb-0 text-nowrap"><b><?php echo $fullname ?></b></h4>
-                                                <p class="mb-0"><?php echo $username ?></p>
-                                                <div class="text-muted"><small>Last Online : <?php echo $date . " - " . $time ?></small></div>
-                                                <div class="mt-3">
-                                                    <form action="edit_picture.php" method="POST" enctype="multipart/form-data">
-                                                        <!-- Select Image File to Upload: -->
-                                                        <input id="profile_picture" type="file" name="file">
-                                                        <!-- <input type="submit" name="submit" value="Upload"> -->
-                                                        <button class="btn btn-primary" type="submit">
-                                                            <i class="fa fa-fw fa-camera"></i>
-                                                            <span>Change Photo</span>
-                                                        </button>
-                                                    </form>
-                                                </div>
-                                            </div>
-                                            <!-- <div class="text-center text-sm-right">
-                                                <span class="badge badge-secondary">administrator</span>
-                                                <div class="text-muted"><small>Joined 09 Dec 2017</small></div>
-                                            </div> -->
-                                        </div>
-                                    </div>
-                                    <!-- <ul class="nav nav-tabs">
-                                        <li class="nav-item"><a href="" class="active nav-link">Settings</a></li>
-                                    </ul> -->
+                                                <!-- <ul class="nav nav-tabs">
+                                                    <li class="nav-item"><a href="" class="active nav-link">Settings</a></li>
+                                                </ul> -->
+                                  
                                     <div class="tab-content pt-3">
                                         <div class="tab-pane active">
                                             <form class="form" action="edit_profile.php" method="POST">
@@ -212,7 +218,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                 <div class="row">
                                                     <div class="col-12 col-sm-6 mb-3">
                                                         <div class="mb-2">
-                                                            <h5>Change Password</h5>
+                                                            <h5><?php echo"Change Password"?></h5>
                                                         </div>
                                                         <div class="row">
                                                             <div class="col">
@@ -262,15 +268,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                                         </div>
                                                     </div> -->
                                                 </div>
-                                                <div class="row">
-                                                    <div class="col d-flex justify-content-end">
-                                                        <button class="btn btn-primary" type="submit">Save Changes</button>
-                                                    </div>
-                                                </div>
+                                                
                                             </form>
 
                                         </div>
                                     </div>
+                               
                                 </div>
                             </div>
                         </div>
@@ -278,7 +281,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     <div class="col-12 col-md-3 mb-3">
                         <div class="card mb-3">
-                            <div class="card-body">
+                            <div id="logout"class="card-body">
                                 <div class="px-xl-3">
                                     <button class="btn btn-block btn-secondary" onclick="window.location.href='logout.php'">
                                         <i class="fa fa-sign-out"></i>
@@ -286,7 +289,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     </button>
                                 </div>
                             </div>
-                            <div class="card-body">
+                            <div id="Profile" class="card-body">
                                 <div class="px-xl-3">
                                     <button class="btn btn-block btn-secondary" onclick="window.location.href='profile.php'">
                                         <i class="fa fa-arrow-circle-o-left"></i>
@@ -294,7 +297,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                     </button>
                                 </div>
                             </div>
-                            <div class="card-body">
+                            <div id="Home"class="card-body">
                                 <div class="px-xl-3">
                                     <button class="btn btn-block btn-secondary" onclick="window.location.href='home.php'">
                                         <i class="fa fa-arrow-circle-o-left"></i>
